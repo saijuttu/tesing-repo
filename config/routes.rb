3d6fe4d1e11e4e143b961f77bdddd6bd7e83a3e1
Sai Juttu <saijuttu@tamu.edu>
2023-02-06 19:46:45 -0600

    edited items

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,7 @@ Rails.application.routes.draw do
     end
   end
 
-  # get 'books/index'
-  # get 'books/new'
-  # get 'books/edit'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
